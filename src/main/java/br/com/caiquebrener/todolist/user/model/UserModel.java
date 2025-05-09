5be9321563a8326ef06e2bfdc9ae64c670dd2984
caique-brener <brenercaique0806@gmail.com>
2025-05-09 19:32:42 -0300

    feat: update model to use lombok

--- src/main/java/br/com/caiquebrener/todolist/user/model/UserModel.java
+++ src/main/java/br/com/caiquebrener/todolist/user/model/UserModel.java
@@ -1,38 +1,13 @@
 package br.com.caiquebrener.todolist.user.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserModel {
 
     private String userName;
     private String name;
     private String password;
-
-    public UserModel(String userName, String name, String password) {
-        this.userName = userName;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
