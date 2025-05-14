24920c27a099284c68480e69193888720ba18547
caique-brener <brenercaique0806@gmail.com>
2025-05-13 22:00:05 -0300

    feat: create new endpoint to get all tasks by user

--- src/main/java/br/com/caiquebrener/todolist/task/controller/TaskController.java
+++ src/main/java/br/com/caiquebrener/todolist/task/controller/TaskController.java
@@ -6,12 +6,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,12 +28,18 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         task.setIdUser((UUID) idUser);
         var currentDate = LocalDateTime.now();
-        if (currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getEndAt())){
+        if (currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The date is not valid");
         }
-        if (task.getStartAt().isAfter(task.getEndAt())){
+        if (task.getStartAt().isAfter(task.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The initial date cannot be later tha the end date");
         }
         return ResponseEntity.status(HttpStatus.OK).body(taskRepository.save(task));
+    }
+
+    @GetMapping
+    public List<TaskModel> listTask(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        return taskRepository.findByIdUser((UUID) idUser);
     }
 }
--- src/main/java/br/com/caiquebrener/todolist/task/repository/ITaskRepository.java
+++ src/main/java/br/com/caiquebrener/todolist/task/repository/ITaskRepository.java
@@ -3,7 +3,9 @@ package br.com.caiquebrener.todolist.task.repository;
 import br.com.caiquebrener.todolist.task.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
