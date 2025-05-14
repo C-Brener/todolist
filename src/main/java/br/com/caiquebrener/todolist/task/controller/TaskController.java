52d8f5670392a695340a3c28733a3e9c880cb560
caique-brener <brenercaique0806@gmail.com>
2025-05-13 22:26:59 -0300

    feat: create update flow

--- src/main/java/br/com/caiquebrener/todolist/task/controller/TaskController.java
+++ src/main/java/br/com/caiquebrener/todolist/task/controller/TaskController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/tasks/")
 public class TaskController {
 
     @Autowired
@@ -37,9 +39,17 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(taskRepository.save(task));
     }
 
-    @GetMapping
+    @GetMapping("get-tasks")
     public List<TaskModel> listTask(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         return taskRepository.findByIdUser((UUID) idUser);
+    }
+
+    @PutMapping("update/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId(id);
+        return taskRepository.save(taskModel);
     }
 }
